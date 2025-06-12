3d065dbe935e2c83d377c0438002780cf716eeb3
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-06-12 23:32:29 +0200

    feat: add backend and frontend folders

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
+
 app.use(express.json());
-app.use(require("cors")());
-
-app.get("/api/test", (_req, res) => res.json({ msg: "Funciona 🎉" }));
-
-app.listen(3000, () => console.log("✅ Backend en http://localhost:3000"));
+app.use(cors());
+app.get("/", (_req, res) => res.json({ msg: "Funciona" }));
+app.listen(3000, () => console.log("Backend en http://localhost:3000"));

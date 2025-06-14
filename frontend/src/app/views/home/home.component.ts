d0e62bf90fee0dc64b790e89e70b33ed835e57f7
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-06-14 14:22:11 +0200

    feat: create form first version

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -3,10 +3,11 @@ import { Router } from '@angular/router';
 import { Store } from '../../core/interfaces/store.model';
 import { StoreService } from '../../core/services/store.service';
 import { StoreListComponent } from "../../features/store-list/store-list.component";
+import { FormStoreComponent } from '../../features/form-store/form-store.component';
 
 @Component({
   selector: 'app-home',
-  imports: [StoreListComponent],
+  imports: [StoreListComponent, FormStoreComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

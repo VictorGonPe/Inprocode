c3f0e69254f0b7698e1c298d62751e60a566ceaf
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-06-15 22:10:04 +0200

    feat: create graphics components and add styles

--- frontend/src/app/features/chart-store/chart-store.component.ts
+++ frontend/src/app/features/chart-store/chart-store.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { Chart } from 'chart.js/auto'; // <-- importante usar 'auto'
 
 @Component({
   selector: 'app-chart-store',
+  standalone: true,
   imports: [],
   templateUrl: './chart-store.component.html',
   styleUrl: './chart-store.component.scss'
 })
-export class ChartStoreComponent {
+export class ChartStoreComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+   
+    new Chart('barCanvas', {
+      type: 'bar',
+      data: {
+        labels: ['Semana 1', 'Semana 2', 'Semana 3'],
+        datasets: [
+          {
+            label: 'Visitantes únicos',
+            data: [90, 200, 180],
+            backgroundColor: '#36A2EB',
+          },
+          {
+            label: 'Visitantes totales',
+            data: [140, 400, 350],
+            backgroundColor: '#f9c74f',
+          },
+        ]
+      },
+      options: {
+        responsive: true
+      }
+    });
+
+  
+    new Chart('donutCanvas', {
+      type: 'doughnut',
+      data: {
+        labels: ['V. Únicos', 'V. Totales', 'Impresiones del perfíl'],
+        datasets: [
+          {
+            data: [400, 800, 1200],
+            backgroundColor: ['#f9c74f', '#36A2EB', '#FF6384'],
+          }
+        ]
+      },
+      options: {
+        responsive: true
+      }
+    });
+  }
 }

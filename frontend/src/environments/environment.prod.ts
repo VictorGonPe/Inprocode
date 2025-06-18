5bb26d30528a6ab3d39cf60678d9630ecd425cb7
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-06-18 18:47:30 +0200

    update git ignore add environment.prod

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  MI_MAPBOX_TOKEN: 'pk.eyJ1IjoidmljdG9yZ3A4MiIsImEiOiJjbWJ3cDN4Z3kwdG03MmxzOHphcXkzZmwwIn0.0q4X1Ck3HYFsZuZE-TwvBw',
+  apiUrl:'https://inprocode.onrender.com/api/stores'
+};

2437fb7f9eb1aff08ab33bda76628f51995dfbc3
VictorGonPe <victor.gonzalez.perez.1982@gmail.com>
2025-06-18 19:16:31 +0200

    Configure CORS to allow request from vercel

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,28 @@ const storeRoutes = require('./routes/store.routes');
 const connectDB = require('./config/db');
 const cors = require('cors');
 
+const allowedOrigins = [
+  'http://localhost:4200',
+  'https://inprocode.vercel.app'
+];
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+}));
+
 
 connectDB();
 
